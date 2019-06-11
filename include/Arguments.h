2b662832a12673405ddb0c3879e2f9d906add528
alexge50 <alexge50.x@gmail.com>
2019-06-11 15:55:13 +0300

    arguments for functions

---
+++ include/Arguments.h
@@ -0,0 +1,36 @@
+//
+// Created by alex on 6/11/19.
+//
+
+#ifndef VARIABLE_ARGS_DYNAMIC_LOADING_ARGUMENTS_H
+#define VARIABLE_ARGS_DYNAMIC_LOADING_ARGUMENTS_H
+
+#include <vector>
+#include <variant>
+#include <unordered_map>
+
+using Type = std::variant<int, float>;
+
+using Arguments = std::unordered_map<std::string, Type>;
+
+using Function = Type(*)(const Arguments&);
+
+struct FunctionInfo
+{
+    std::string name;
+    std::unordered_map<std::string, std::string> argumentTypes;
+    std::string returnType;
+    Function function;
+};
+
+using FunctionsInfo = std::vector<FunctionInfo>;
+
+using ExportedSymbols = FunctionsInfo(*)();
+
+#ifdef __WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __attribute__((__visibility__("default")))
+#endif
+
+#endif //VARIABLE_ARGS_DYNAMIC_LOADING_ARGUMENTS_H
